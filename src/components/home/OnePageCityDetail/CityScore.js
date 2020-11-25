4f6191ba9a65113086a59bc78db385efb0988b8a
Fellype <fellype.ads@gmail.com>
2020-11-25 02:47:50 -0300

    score com modal funcionando dinamicamente

--- src/components/home/OnePageCityDetail/CityScore.js
+++ src/components/home/OnePageCityDetail/CityScore.js
@@ -1,21 +1,35 @@
 import React, { useState, useEffect } from "react";
+import Modal from "react-bootstrap/Modal";
 
 import "./CityScore.css";
+import CategoryDetail from "./CategoryDetail";
 
 function CityScore(props) {
   const [state, setState] = useState([]);
+  const [currentlyItem, setCurrentlyItem] = useState({
+    status: false,
+    name: "",
+  });
 
   useEffect(() => {
     setState([...props.scores]);
   }, [props]);
 
+  function handleShow(event) {
+    event.preventDefault();
+    setCurrentlyItem({ status: true, name: event.target.name });
+  }
+  function handleClose() {
+    setCurrentlyItem({ ...currentlyItem, status: false });
+  }
+
   return (
     <div className="score">
       <h4>LIFE QUALITY SCORE</h4>
       <div className="scoreData">
-        {state.map((elem) => {
+        {state.map((elem, idx) => {
           return (
-            <div className="scoreTopic">
+            <div className="scoreTopic" key={idx}>
               <div className="scoreLabel">
                 <h6>{elem.name}</h6>
               </div>
@@ -30,13 +44,18 @@ function CityScore(props) {
                     width: `${Math.round(elem.score_out_of_10)}0%`,
                   }}
                   role="progressbar"
-                  aria-valuenow={`$Math.round(elem.score_out_of_10)}0`}
+                  aria-valuenow={Math.round(elem.score_out_of_10) * 10}
                   aria-valuemin="0"
                   aria-valuemax="100"
                 ></div>
               </div>
               <div className="btn btn-light btn-sm scoreButton">
-                <button type="button" className="btn">
+                <button
+                  type="button"
+                  className="btn btn-outline-success"
+                  name={elem.name}
+                  onClick={handleShow}
+                >
                   Details
                 </button>
               </div>
@@ -44,6 +63,16 @@ function CityScore(props) {
           );
         })}
       </div>
+      {currentlyItem.status ? (
+        <CategoryDetail
+          id={props.id}
+          apper={currentlyItem.status}
+          currentlyName={currentlyItem.name}
+          closeModel={handleClose}
+        />
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
